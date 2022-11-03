20ddc293affb32440f9941d59a1be3aae36145d1
RobinGH00 <82035246+RobinGH00@users.noreply.github.com>
2022-11-03 22:24:07 +0100

    Add files via upload

---
+++ Messaging-system/README.md
@@ -0,0 +1,45 @@
+
+# Demo instructions
+1. pip3 install -r requirements. 
+2. Flask Run
+3. Navigate to 127.0.0.1:5000
+4. Register a new user and login
+
+
+# Features
+* Instant messages between user accounts.
+* A sender and one recepint
+* Sends a message with some content, can reply to the same message
+* A read /unread inbox with timestamp using MomentJS and Flask-moment. 
+* Delete messages / all messages in that conversation
+
+# Code analysis
+* Comments in code app.py
+
+
+# Security Considerations
+* Authentication
+  Web sockets do not perform any authentication. Even if you do have authentication in place, it is still possible to performe som esort of MITM attack, simply by modefing the clinet-sice javascript to try to intercept event channels that should not be accessed. These channels are unprotected, so if someone gets the event channel ID. its easy to eavesdrop.
+  If authentication on the socket is required, it should be done via a cookie or token in the header, or passed as an argument during the connection.
+
+  his application will listen for specific events emitted to the random string ID for the messaging thread. Once the socket detects the emitted event it understands that a new messages has arrived for that thread, from a regular Ajax request is sent to get the new message data. By using ajax. we can perform full server-side authentication of the client to verify its identity before sending any data back. In this way, if we somehow obtain a randomly generated ID and attempt to sniff a particular event channel, we will only see notifications of new incoming messages. Hence, no sensitive information will be exposed.
+
+* Cross Origin Resource Sharing
+  The Flask-SocketIO library used has built-in CORS support. This will prevent external websites from accessing the websocket and potentially using server resources.
+
+# Questions
+* Threat model – who might attack the application? What can an attacker do? What damage could be done (in terms of confidentiality, integrity, availability)? Are there limits to what an attacker can do?  Are there limits to what we can sensibly protect against?
+    * Typical attackers might be unathorized users which is a threat to the confidentiality and the integrity as the attacker is not welcome. Also if the attacker gains access to the database they can     modify or delete the database. Something like this will threaten the availibilty as authorized can not longer log in to the web application. To protect the database, its important to have a backup stored locally. 
+
+* What are the main attack vectors for the application?
+    * The main attack vectors for this application is sql injections. This is a huge cyber risk as the database stores username, password and private messages. Attackers gain unauthorized access to the system and steal sensitive data or modify the data. 
+
+* What should we do (or what have you done) to protect against attacks?
+    * The only sure way to prevent sql attacks is with input validation and parametrized quieris including prepared statements. The application code should never use the input directly. 
+
+* What is the access control model?
+    * The access control model enables you to control the ability of a process to access securable objects or to preform various system administration tasks. 
+
+* How can you know that you security is good enough? (traceability)
+    * One way to ensure good enough security and data traceability is to complete a study beginning to end on a closed system or platform. Some other factors for good security is: Logging and backup. Applications write event log data to the filesystem or a database, and have a backup of the last safe-known state. 
+
---
+++ Messaging-system/app copy.py
@@ -0,0 +1,340 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, abort
+from flask_sqlalchemy import SQLAlchemy
+from uuid import uuid4
+from flask_socketio import SocketIO, emit, Namespace
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+import random, string
+from sqlalchemy import exists, case, distinct
+from flask_moment import Moment
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'mY_sEcreT_keY'
+
+
+database = SQLAlchemy(app)
+socketio = SocketIO(app)
+login_manager = LoginManager(app)
+moment = Moment(app)
+
+
+login_manager.login_view = 'login'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+#Random string generator for message and thread id
+def randomstring(type, pmthread=None):
+	letters = string.ascii_lowercase
+	randstr =  ''.join(random.choice(letters) for i in range(8))
+
+	if pmthread:
+		if not database.session.query(exists().where(Message.thread_id == randstr)).scalar():
+			return randstr
+		else:
+			randomstring(type=Message, pmthread=True)
+
+	if not database.session.query(exists().where(type.url == randstr)).scalar():
+		return randstr
+	else:
+		randomstring(type=type)
+
+#Database models
+class User(database.Model, UserMixin):
+    __tablename__ = 'users'
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String(80))
+    password = database.Column(database.String(80))
+    websocket_id = database.Column(database.String, unique=True, index=True)
+
+
+class Message(database.Model):
+    __tablename__ = 'messages'
+    id = database.Column(database.Integer(), primary_key=True)
+    url = database.Column(database.String())
+    sender_id = database.Column(database.String())
+    recipient_id = database.Column(database.String())
+    subject = database.Column(database.String())
+    body = database.Column(database.String())
+    timestamp = database.Column(database.DateTime)
+    read = database.Column(database.Boolean(), default=False)
+    thread_id = database.Column(database.String())
+    sender_del = database.Column(database.Boolean())
+    recipient_del = database.Column(database.Boolean())
+
+#App view routes
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username').lower()
+    password = request.form.get('password')
+    user = User.query.filter_by(username=username).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Username or Password is inncorect, try again.')
+        return redirect(url_for('login'))
+    login_user(user)
+    return redirect(url_for('messages'))
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['POST'])
+def register_post():
+    username = request.form.get('username').lower()
+    password = request.form.get('password')
+    user = User.query.filter_by(username=username).first()
+
+    if user:
+        flash('Username is taken')
+        return redirect(url_for('register'))
+
+    new_user = User(username=username, password=generate_password_hash(password, method='sha256'), websocket_id=uuid4().hex)
+    database.session.add(new_user)
+    database.session.commit()
+    return redirect(url_for('login'))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/messages/', methods=['POST', 'GET'])
+@login_required
+def messages():
+	if request.args.get('thread_id'):
+		if not database.session.query(Message).filter(Message.thread_id == request.args.get('thread_id'), Message.recipient_id == current_user.username) \
+		or not database.session.query(Message).filter(Message.thread_id == request.args.get('thread_id'), Message.sender_id == current_user.username):
+			abort(404)
+
+		message_thread_sender = database.session.query(Message).filter(Message.thread_id == request.args.get('thread_id'), Message.sender_id == current_user.username, Message.sender_del == False)
+		message_thread_recipient = database.session.query(Message).filter(Message.thread_id == request.args.get('thread_id'), Message.recipient_id == current_user.username, Message.recipient_del == False)
+		message_thread = message_thread_sender.union(message_thread_recipient).order_by(Message.timestamp.asc())
+
+		if not message_thread.count():
+				abort(404)
+
+		thread_count = len(message_thread.all())
+		if thread_count <= 5:
+			offset = 0
+		else:
+			offset = thread_count-5
+		message_thread_paginated = message_thread.offset(offset).limit(5)
+
+		if request.args.get('fetch'): 
+			fetch_last_query = database.session.query(Message).filter(Message.url == request.args.get('fetch')).one()
+			testq = message_thread_sender.union(message_thread_recipient).order_by(Message.timestamp.asc()).filter(Message.message_id < fetch_last_query.id) 
+			testq_count = testq.count()
+			if testq_count-5 < 0:
+				offsetcnt = 0
+			else:
+				offsetcnt = testq_count-5
+			testq = testq.offset(offsetcnt)
+
+			fetched_messages = render_template('new_message.html', message_thread=testq)
+			return {'status': 200, 'fetched_messages': fetched_messages, 'offsetcnt':offsetcnt}
+
+		for message in message_thread:
+			if current_user.username == message.recipient_id:
+				if message.read == False:
+					message.read = True
+					database.session.commit()
+
+		if current_user.username == message_thread[0].sender_id:
+			recip = message_thread[0].recipient_id
+		else:
+			recip = message_thread[0].sender_id
+
+		if not database.session.query(Message).filter(Message.recipient_id == current_user.username, Message.read == False).all():
+			socketio.emit(current_user.websocket_id+'_notify', {'type':'mailbox', 'notify':'false'}, namespace='/messages')
+
+		socketio.emit(current_user.websocket_id+'_notify', {'type':'thread', 'notify':'false', 'thread_id':request.args.get('thread_id')}, namespace='/messages')
+
+		return render_template('read_message_thread.html', message_thread=message_thread_paginated, thread_id=request.args.get('thread_id'),\
+								recip=recip, thread_count=thread_count)
+
+
+	else:
+		page = request.args.get('page', 1, type=int)
+
+		unread_messages = database.session.query(Message).filter(Message.recipient_id == current_user.username, Message.recipient_del == False).order_by(Message.timestamp.desc())
+		unread_ids = {}
+
+		for message in unread_messages:
+			if not unread_ids.get(message.thread_id):
+				unread_ids[message.thread_id] = len(unread_ids)+1
+		if not unread_ids:
+			sort_order = None
+		else:
+			sort_order = case(value=Message.thread_id, whens=unread_ids).asc()
+
+		thread_list = []
+		message_thread_list = []
+		for message in unread_messages:
+			if message.thread_id not in thread_list:
+				thread_list.append(message.thread_id)
+				message_thread_list.append(message.url)
+
+		message_threads = unread_messages.filter(Message.url.in_(message_thread_list)).order_by(sort_order)
+		unread_threads = unread_messages.filter(Message.read == False).order_by(Message.timestamp.desc()).all()
+
+		if unread_threads:
+			unread_threads_list = []
+			for message in unread_threads:
+				unread_threads_list.append(message.thread_id)
+		else:
+			unread_threads_list = []
+		message_threads = message_threads.paginate(page, 5, False)
+
+		if page > 1:
+			paged_threads = render_template('new_thread.html', messages=message_threads.items, unread_threads_list=unread_threads_list)
+			if not unread_messages.filter(Message.url.in_(message_thread_list)).order_by(sort_order).paginate(page+1, 5, False).items:
+				fetch_button = 'false'
+			else:
+				fetch_button = 'true'
+			return {'status':200, 'threads':paged_threads, 'fetch_button':fetch_button}
+
+		if len(message_thread_list) > 5:
+			fetch_button = 'true'
+		else:
+			fetch_button = 'false'
+
+		return render_template('messages.html', messages=message_threads.items, unread_threads_list=unread_threads_list, fetch_button=fetch_button)
+
+
+
+@app.route('/messages/socket/', methods=['POST', 'GET']) 
+@login_required
+def message_socket():
+	message = database.session.query(Message).filter(Message.url == request.args.get('url')).all()
+
+	if not message:
+		abort(404)
+	if current_user.username == message[0].recipient_id or current_user.username == message[0].sender_id:
+		pass
+	else:
+		return {'status': 401}
+	if current_user.username == message[0].recipient_id and request.args.get('read'):
+		message[0].read = True 
+		database.session.commit()
+		if not database.session.query(Message).filter(Message.recipient_id == current_user.username, Message.read == False, Message.recipient_del == False).all():
+			socketio.emit(current_user.websocket_id+'_notify', {'type':'mailbox', 'notify':'false'}, namespace='/messages')
+	if request.args.get('read'):
+		socketio.emit(current_user.websocket_id+'_notify', {'type':'thread', 'notify':'false', 'thread_id':message[0].thread_id}, namespace='/messages')
+		render_message = render_template('new_message.html', message_thread=message)
+		return {'status':200, 'message':render_message}
+	else:
+		render_thread = render_template('new_thread.html', messages=message, unread_threads_list=[message[0].thread_id])
+		return {'status':200, 'thread':render_thread, 'thread_id':message[0].thread_id}
+
+
+
+
+
+@app.route('/messages/new/', methods=['POST', 'GET'])
+@login_required
+def sendmessage():
+	if request.method == 'GET':
+		return render_template('send_message.html')
+	if request.method == 'POST':
+		if request.json.get('body') == '' or request.json.get('body') == None or len(request.json.get('subject')) > 70:
+			return {'status':418}
+		if request.json.get('thread_id'):
+			if database.session.query(Message).filter(Message.thread_id == request.json.get('thread_id'), Message.sender_id == current_user.username).all() or \
+				database.session.query(Message).filter(Message.thread_id == request.json.get('thread_id'), Message.recipient_id == current_user.username).all():
+				pass
+			else:
+				return {'status': 418}
+		if not database.session.query(User).filter(User.username == request.json.get('recipient_id').lower()).first():
+			return {'error':'No user exists with that username.'}
+
+		url = randomstring(type=Message)
+		timestamp=datetime.utcnow()
+
+		if request.json.get('thread_id'):
+			thread_id = request.json.get('thread_id')
+			thread_query = database.session.query(Message).filter(Message.thread_id == thread_id)
+			subject = thread_query.order_by(Message.timestamp.desc()).first().subject
+		else:
+			thread_id = randomstring(type=Message, pmthread=True)
+			subject = request.json.get('subject')
+
+		new_message = Message(sender_id=current_user.username, recipient_id=request.json.get('recipient_id').lower(), subject=subject, body=request.json.get('body'), url=url, \
+						 thread_id=thread_id, timestamp=timestamp, sender_del=False, recipient_del=False)
+		database.session.add(new_message)
+		database.session.commit()
+
+		recipient_websocket_id = database.session.query(User).filter(User.username == request.json.get('recipient_id').lower()).one().websocket_id
+		socketio.emit(recipient_websocket_id+'_newmsg', {'message_url' : url}, namespace='/messages') 
+		socketio.emit(current_user.websocket_id+'_newmsg', {'message_url' : url}, namespace='/messages') 
+		socketio.emit(thread_id, {'message_url' : url}, namespace='/messages')
+
+		return {'status': 200}
+
+
+@app.route('/messages/delete/', methods=['POST'])
+def message_delete():
+	if not current_user.is_authenticated:
+		return {'status':401}
+
+	if request.json.get('type') == 'thread':
+		messages = database.session.query(Message).filter(Message.thread_id == request.json.get('url')).all()
+		for message in messages:
+			if message.recipient_id == current_user.username:
+				message.recipient_del = True
+				database.session.commit()
+			if message.sender_id == current_user.username:
+				message.sender_del = True
+				database.session.commit()
+
+		sender_messages = database.session.query(Message).filter(Message.thread_id == message.thread_id, Message.sender_id == current_user.username, Message.sender_del == False)
+		recipient_messages = database.session.query(Message).filter(Message.thread_id == message.thread_id, Message.recipient_id == current_user.username, Message.recipient_del == False)
+		total_threads = sender_messages.union(recipient_messages).distinct(Message.thread_id).count()
+
+		socketio.emit(current_user.websocket_id+'_notify_deletion', {'type':'thread', 'thread_id':request.json.get('url'), 'total_threads':total_threads}, namespace='/messages')
+		flash('Message deleted', 'success')
+		return {'status': 200}
+
+	if request.json.get('type') == 'message':
+		message = database.session.query(Message).filter(Message.url == request.json.get('url')).first()
+
+		if message.recipient_id == current_user.username:
+			message.recipient_del = True
+			database.session.commit()
+
+		if message.sender_id == current_user.username:
+			message.sender_del = True
+			database.session.commit()
+
+		sender_messages = database.session.query(Message).filter(Message.thread_id == message.thread_id, Message.sender_id == current_user.username, Message.sender_del == False)
+		recipient_messages = database.session.query(Message).filter(Message.thread_id == message.thread_id, Message.recipient_id == current_user.username, Message.recipient_del == False)
+		total_messages = sender_messages.union(recipient_messages).count()
+
+		socketio.emit(current_user.websocket_id+'_notify_deletion', {'type':'message', 'message_url':request.json.get('url'), 'thread_id':message.thread_id, 'total_messages':total_messages}, namespace='/messages')
+		return {'status': 200}
+
+
+
+if __name__ == "__main__":
+    socketio.run(app)
+	
+	
